ebabeeb735d091f4ac1063f8b442fd9a53d23591
Gergo <kapusi.gergo.levente@students.jedlik.eu>
2025-03-13 20:27:01 +0100

    Sorting

--- app/api/posts/route.ts
+++ app/api/posts/route.ts
@@ -13,27 +13,27 @@ const CORS = {
 export async function GET(req: NextRequest): Promise<NextResponse> {
 
     const postID = req.nextUrl.searchParams.get("postID");
+
     if (postID) {
         const data = await prisma.post.findUnique({
             where: {
                 postID: postID
+            },
+            include: {
+                Ratings: true
             }
         });
         return new NextResponse(
             JSON.stringify({ data }),
             {
                 status: 200,
-                headers: CORS,// print("hjfkahflkh [0], [1]", v1, v2) 
+                headers: CORS,
             }
         );
     }
 
-
-    const data = await prisma.post.findMany({
-        include: {
-            Ratings: true
-        }
-    })
+    const sort = req.nextUrl.searchParams.get("sort");
+    const data = await handleSort(sort);
     const response = new NextResponse(
         JSON.stringify({ data }),
         {
@@ -52,6 +52,71 @@ export async function OPTIONS(): Promise<NextResponse> {
     });
 }
 
+async function handleSort(sort: string | null) {
+    let data;
+    console.log(sort)
+    switch (sort) {
+        case "basic":
+            data = await prisma.post.findMany({
+                include: {
+                    Ratings: true
+                },
+                orderBy: {
+                    postID: "asc"
+                }
+            })
+            break;
+        case "reverse":
+            data = await prisma.post.findMany({
+                include: {
+                    Ratings: true
+                },
+                orderBy: {
+                    postID: "desc"
+                }
+            })
+        case "popular":
+            data = await prisma.post.findMany({
+                include: {
+                    Ratings: true
+                },
+                orderBy: {
+                    Ratings: {
+                        _count: "desc"
+                    }
+                }
+            })
+            break;
+        case "newest":
+            data = await prisma.post.findMany({
+                include: {
+                    Ratings: true
+                },
+                orderBy: {
+                    createdAt: "desc"
+                }
+            })
+            break;
+        case "oldest":
+            data = await prisma.post.findMany({
+                include: {
+                    Ratings: true
+                },
+                orderBy: {
+                    createdAt: "asc"
+                }
+            })
+            break;
+        default:
+            data = await prisma.post.findMany({
+                include: {
+                    Ratings: true
+                }
+            })
+            break;
+    }
+    return data;
+}
 /*
 export async function POST(req: NextRequest): Promise<NextResponse> {
     const formData = await req.formData()
