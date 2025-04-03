44ed7ad6f6ec4439f1c10c334ee559c15d9dc204
Gergo <kapusi.gergo.levente@students.jedlik.eu>
2025-04-03 13:09:22 +0200

    update route.ts

--- app/api/posts/route.ts
+++ app/api/posts/route.ts
@@ -5,7 +5,7 @@ import { NextRequest, NextResponse } from 'next/server';
 const CORS = {
     'Content-Type': 'application/json',
     'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+    'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS, PATCH',
     'Access-Control-Allow-Headers': 'Content-Type, Authorization',
     'Access-Control-Allow-Credentials': 'true',
 }
